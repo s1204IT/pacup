a330af40e82b1b4ffdbfa8641d71fb33b0a26b06
PengiNN <103301288+PengiNN@users.noreply.github.com>
2022-05-29 09:13:03 +0900

    Add files via upload

---
+++ pacup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+if [`whoami`!='root']; then
+  echo "管理者権限で実行してください"
+  exit
+fi
+
+function apt_ () {
+  sudo apt update
+
+  while getopts ":y" optKey; do
+    case "$optKey" in
+      y)
+        sudo apt -y upgrade
+        exit
+        ;;
+    esac
+  done
+
+  sudo apt upgrade
+
+  exit
+}
+
+apt_
