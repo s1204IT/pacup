9d6160e3675664441f497983eecf68f1bde0a612
PengiNN <103301288+PengiNN@users.noreply.github.com>
2022-10-14 19:01:23 +0900

    Update pacup.sh
    Add -f option

--- pacup.sh
+++ pacup.sh
@@ -4,6 +4,7 @@ function usage() {
 Usage: $(basename "$0") [OPTION]...
     -h 		Display help
     -y		Skip input 'y'
+    -f      Run full-upgrade
 EOM
 
     exit 2
@@ -14,16 +15,24 @@ function update_y() {
     exit 0
 }
 
+function update_full() {
+    apt update && apt full-upgrade -y
+    exit 0
+}
+
 if [ `whoami` != 'root' ]; then
     echo "管理者権限で実行してください"
     exit 0
 fi
 
-while getopts ":h:y" optKey; do
+while getopts ":h:y:f" optKey; do
     case "$optKey" in
     	y)
     	  update_y
           ;;    	
+        f)
+          upgrade_full
+          ;;
     	'-h'|'--help'|* )
     	  usage
     	  ;;
