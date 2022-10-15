9244f46a140427f6059723328da4438167f69a49
PengiNN <103301288+PengiNN@users.noreply.github.com>
2022-10-15 10:58:11 +0900

    Add files via upload

--- install_pacup.sh
+++ install_pacup.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 if [ `whoami` != 'root' ]; then
   echo "管理者権限で実行してください"
-  exit
+  exit 0
 fi
 
+. /etc/os-release
+
+if [ ${ID} != "debian" ]; then
+    echo ""
+    exit 0
+fi
+
+script_path="$(dirname "$(readlink -f "$0")")"
+
+cd ${script_path}
 sudo cp ./pacup.sh /usr/bin/pacup
 sudo chmod +x /usr/bin/pacup
 echo "Success!"
