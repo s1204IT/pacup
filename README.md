40933cec55059159120903ed172ab08d4658d529
Syuugo <pub@s1204.me>
2022-10-26 19:50:32 +0900

    Major Update

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 # pacup
-Simplify package updates
+***Simplify package updates***
 
-Ubuntu/Debian系のディストリで、パッケージのアップデートをする際に
-```sudo apt update && sudo apt upgrade```　と入力するのを
-```sudo pacup```　といった風に短縮するためのコマンドです.
+Debian系 もしくは Debian派生系のディストリビューションで､ パッケージのアップデートをする際に  
+`sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo snap refresh && flatpak update`  
+と入力するのを `pacup` として**大幅に短縮する**コマンドです｡
 
-```-y```　オプションをつけることで途中のy入力をスキップできます
-
-```-f```　オプションをつけることでfull-upgradeを実行します
+### オプション
+- `-y`: 続行を確認をスキップします
+- `-a`: APTのみ実行します
 
 ## インストール方法
 ```
-git clone https://github.com/PengiNN/pacup
-sudo bash ./pacup/install_pacup.sh
+sudo apt install -y git
+git clone https://github.com/s1204IT/pacup
+sudo ./pacup/install_pacup.sh
 ```
+コピー＆ペーストで実行して下さい｡  
+更新を適用する場合は､ `install_pacup.sh`を実行すると自動で処理されます｡  
+※通常､ クローンされたフォルダが削除されていない場合に限りますが､ 上記と同じコマンドを実行しても処理されます｡
--- pacup.sh
+++ pacup.sh
@@ -1,38 +1,78 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+
+# ヘルプ
 function usage() {
-    cat <<EOM
-Usage: $(basename "$0") [OPTION]...
-    -h 		Display help
-    -y		Skip input 'y'
-    -f		Run full-upgrade
+  cat <<EOM
+使用方法: $(basename "$0") [OPTION]...
+    -a          APTのみ実行
+    -y		'y'の入力をスキップします
+    -h          ヘルプを表示します
 EOM
-
-    exit 2
 }
 
-function update_y() {
-    echo 'Skip input "y"'
-    apt update && apt -y upgrade
-    exit 0
+# オプションの読み取り
+while (($#>0)); do
+  case $1 in
+    a|-a|--apt)
+      PACUP_MOD="apt"
+      ;;
+    y|-y|--yes)
+      PACUP_YES=" -y"
+      ;;
+    h|-h|--help)
+      usage
+      exit 0
+      ;;
+    *)
+      usage
+      exit 1
+      ;;
+  esac
+  # $1が無くなるまで繰り返す
+  shift
+done
+
+function PACUP_SYS() {
+  # install_pacup.sh から書込
+  : #SYS
 }
 
-function update_full() {
-    echo 'Run full-upgrade'
-    apt update && apt -y full-upgrade
-    exit 0
+function PACUP_APT() {
+  sudo apt update
+  sudo apt full-upgrade$PACUP_YES
+  sudo apt autoremove$PACUP_YES
 }
 
-if [ `whoami` != 'root' ]; then
-    echo "管理者権限で実行してください"
-    exit 0
+# APTでの更新のみ実行
+if [ "$PACUP_MOD" == "apt" ]; then
+   PACUP_APT
+  # Root要求が失敗した際にリトライ
+  if [ $? != 0 ]; then
+    echo -e "\nAPTコマンドの実行はRoot(管理者)権限を要求します｡\nもう一度お試しください｡\n"
+    PACUP_APT
+    # リトライも失敗した際にエラーで終了
+    if [ $? != 0 ]; then
+      echo -e "\n権限の昇格に失敗したため､ 実行を終了しました｡"
+      exit 1
+    fi
+  fi
+  exit 0
 fi
 
-while getopts :hyf optKey; do
-    case "$optKey" in
-	y) update_y ;;
-	f) update_full ;;
-    '-h'|'--help'|* ) usage ;;
-    esac
-done
-apt update && apt upgrade
+# APTでの更新
+PACUP_APT
+if [ $? != 0 ]; then
+  echo -e "\nRoot(管理者)権限を要求しています｡\nもう一度お試しください｡\n"
+  PACUP_SYS
+  if [ $? != 0 ]; then
+    echo -e "\n権限の昇格に失敗したため､ 実行を終了しました｡"
+    exit 1
+  fi
+fi
+
+# Flatpakコマンドが存在する場合に install_pacup.sh より書込
+#FPK
+# RootユーザーでのSnapとFlatpakの更新
+PACUP_SYS
 exit 0
